4ee9b40aad8acca35f3e9f12392d48ba025db357
MIKE SHOBE <mikeshobe@yahoo.com>
2015-07-27 16:31:21 -0700

    starting angular

--- server/scraper.js
+++ server/scraper.js
@@ -3,11 +3,14 @@ var request = require('request');
 
 var scrapeController = {
   getData: function(req, res, next) {
-  	var apiObject = {};
-  	var foodItem = 'pizza'; //FIX ME: will be equal to user input
+  	
+    var json = {};
+    json.data = [];
+    var links = [];
+  	var foodItem = 'beef'; //FIX ME: will be equal to user input
   	var url = 'http://allrecipes.com/search/default.aspx?qt=k&rt=r&origin=Recipe+Search+Results&vm=l&p34=SR_ListView&wt=' + foodItem + '&pqt=k&ms=0&fo=0';
     // change URL to any site that you want
-    request(url, function(error, response, html) {
+    request(url, function(error, res, html) {
         if (error){
         	console.log(error);//- want to check for errors first and console log them
         	return response.end();
@@ -15,20 +18,37 @@ var scrapeController = {
         //- look through response's html
         var $ = cheerio.load(html);
         var links = [];
+        var apiObject = {};
+        var counter = 16;
         for (var i = 0; i <= 40; i += 2) {
         	var recipeURL = $("#ctl00_CenterColumnPlaceHolder_rptResults_ctl" + i + "_ucResultContainer_ucRecipe_lnkTitle").attr('href');
         	if (recipeURL){
         		 request(recipeURL, function(error, response, html2){
         			var $ = cheerio.load(html2);
-        			console.log($('#itemTitle').text());
-        			if ($('#totalHoursSpan')) {
-        				console.log($('#totalHoursSpan').text());
+                    apiObject.itemTitle = $('#itemTitle').text();
+        			if ($('#totalHoursSpan').text()) {
+                        apiObject.totalHoursSpan = $('#totalHoursSpan').text();
         			}
-        		});
+                    if ($('#totalMinsSpan').text()) {
+                        apiObject.totalMinsSpan = $('#totalMinsSpan').text();
+                    }
+                    apiObject.aggregateRating = $('div[itemprop="aggregateRating"]').children().children().children().attr('content');
+                    apiObject.reviewsAmount = $('#btnScrollToReview').children().text();
+                    links.push(apiObject);
+                    counter --;
+                    console.log(counter);
+                    if (counter === 0){
+                        // res.send(JSON.stringify(links));
+                        console.log(links);
+                    }
+        		})
+
         	}
+
         }
+        // console.log(links); 
     });
-  }
+    }
 }
 
 module.exports = scrapeController;
