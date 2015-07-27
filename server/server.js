6525493a8decf9fa3530fb14e0c6c8337c320e1f
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-07-26 21:22:59 -0700

    Adds client folder to server script

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 var scraperController = require('./scraper');
 
+app.use(express.static('client'));
 // first sample route
-app.get('/sample', scraperController.getData)
-
+app.get('/sample', scraperController.getData);
 app.listen(3000);
 
 module.exports = app;
