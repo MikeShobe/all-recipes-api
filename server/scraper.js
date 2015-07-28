3c221bf9ec034c837374adeee4ba55b20bdab2df
MIKE SHOBE <mikeshobe@yahoo.com>
2015-07-27 17:28:38 -0700

    api now working

--- server/scraper.js
+++ server/scraper.js
@@ -10,7 +10,7 @@ var scrapeController = {
   	var foodItem = 'beef'; //FIX ME: will be equal to user input
   	var url = 'http://allrecipes.com/search/default.aspx?qt=k&rt=r&origin=Recipe+Search+Results&vm=l&p34=SR_ListView&wt=' + foodItem + '&pqt=k&ms=0&fo=0';
     // change URL to any site that you want
-    request(url, function(error, res, html) {
+    request(url, function(error, response, html) {
         if (error){
         	console.log(error);//- want to check for errors first and console log them
         	return response.end();
@@ -18,35 +18,44 @@ var scrapeController = {
         //- look through response's html
         var $ = cheerio.load(html);
         var links = [];
-        var apiObject = {};
-        var counter = 16;
+        
+        var counter = 21;
         for (var i = 0; i <= 40; i += 2) {
-        	var recipeURL = $("#ctl00_CenterColumnPlaceHolder_rptResults_ctl" + i + "_ucResultContainer_ucRecipe_lnkTitle").attr('href');
+            var num = i;
+            if (num < 10){
+                num = '0' + num;
+            }
+        	var recipeURL = $("#ctl00_CenterColumnPlaceHolder_rptResults_ctl" + num + "_ucResultContainer_ucRecipe_lnkTitle").attr('href');
         	if (recipeURL){
-        		 request(recipeURL, function(error, response, html2){
-        			var $ = cheerio.load(html2);
-                    apiObject.itemTitle = $('#itemTitle').text();
+                console.log(i);
+        		 request(recipeURL, function(error, response2, html2){
+        			var apiObject = {};
+                    var $ = cheerio.load(html2);
+                    apiObject.RecipeName = $('#itemTitle').text();
+                    apiObject.CookTime = '';
         			if ($('#totalHoursSpan').text()) {
-                        apiObject.totalHoursSpan = $('#totalHoursSpan').text();
+                        apiObject.CookTime += $('#totalHoursSpan').text();
         			}
                     if ($('#totalMinsSpan').text()) {
-                        apiObject.totalMinsSpan = $('#totalMinsSpan').text();
+                        if (apiObject.CookTime){
+                            apiObject.CookTime += ' ';
+                        }
+                        apiObject.CookTime += $('#totalMinsSpan').text();
                     }
-                    apiObject.aggregateRating = $('div[itemprop="aggregateRating"]').children().children().children().attr('content');
-                    apiObject.reviewsAmount = $('#btnScrollToReview').children().text();
+                    apiObject.Rating = $('div[itemprop="aggregateRating"]').children().children().children().attr('content');
+                    apiObject.ReviewsAmount = $('#btnScrollToReview').children().text();
                     links.push(apiObject);
                     counter --;
-                    console.log(counter);
                     if (counter === 0){
-                        // res.send(JSON.stringify(links));
-                        console.log(links);
+                        res.send(JSON.stringify(links));
                     }
         		})
 
         	}
 
         }
-        // console.log(links); 
+
+
     });
     }
 }
